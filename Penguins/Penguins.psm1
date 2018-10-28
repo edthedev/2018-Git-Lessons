21e5b52e069aa70639a4fc6d773c08a6dab3c6f2
Edward Delaporte <edward.delaporte@busey.com>
2018-10-28 17:20:58 -0500

    Penguin script works.

--- Penguins/Penguins.psm1
+++ Penguins/Penguins.psm1
@@ -1,13 +1,25 @@
 
+<#
 
+.EXAMPLE 
+PS C:\VSO\2018-Git-Lessons> remove-module penguins
+PS C:\VSO\2018-Git-Lessons> import-module ./penguins
+PS C:\VSO\2018-Git-Lessons> Get-Command -Module Penguins
+
+CommandType     Name                                               Version    Source
+-----------     ----                                               -------    ------
+Function        Get-Penguin                                        0.0        penguins
+
+#>
 function Get-Penguin {
     Param(
         [int]$index
     )
-    $allPenguinText = Get-Content -Path Penguins.txt
-    $allPenguins = $allPenguinText.Split("***")
-    $selectedPenguin = $allPenguins[$index]
-    return $selectedPenguin
+    $allPenguinText = Get-Content -Path ./Penguins.txt
+    $allPenguins = $allPenguinText -Join "|"
+    $pArray = $allPenguins.Split("***")
+    $selectedPenguin = $pArray[$index].Split("|")
+    $selectedPenguin
 }
 
 Export-ModuleMember -Function Get-Penguin
